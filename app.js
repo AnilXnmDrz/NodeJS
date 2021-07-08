0e35fbe3acb0aed705adaa8fb04f0dd6352967a5
anil <anilsonam98@gmail.com>
2021-07-08 16:21:46 +0530

    chalk module

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const validator=require('validator')
+const chalk=require('chalk')
+
 const n=require('./note.js')
 // urno
 console.log(n())
 
 console.log(validator.isEmail('anilsunamgmail.com'))
+// console.log(chalk.redBright.bold("Edited by chalk")+ chalk.bold("4.1"))
+// console.log(chalk.keyword('orange')('orange color'))
+// console.log(chalk.rgb(123,45,67).underline('ubterlined reddish color'))
+const error =chalk.bold.red
+const warning= chalk.keyword('orange')
+const success=chalk.green.inverse
+
+console.log('\t'+error('error report'))
+console.log('\t'+warning('warning report'))
+console.log('\t'+success('success report'))
